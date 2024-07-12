54c17c8d838466b77b4f87377c73dec0662159e4
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-07-13 00:53:11 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\CitizenController;
+Route::get('/citizens', [CitizenController::class, 'index']);
+Route::get('/citizens/{id}', [CitizenController::class, 'show']);
+Route::post('/citizens', [CitizenController::class, 'store']);
+Route::put('/citizens/{id}', [CitizenController::class, 'update']);
+Route::delete('/citizens/{id}', [CitizenController::class, 'delete']);
+
 
 Route::get('/', function () {
     return view('welcome');
