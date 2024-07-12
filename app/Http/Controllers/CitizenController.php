6bede65835343a1ff8ca15169adda43b32dc6993
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-07-12 22:26:20 +0300

    Update CitizenController.php

--- app/Http/Controllers/CitizenController.php
+++ app/Http/Controllers/CitizenController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Citizen;
 use Illuminate\Http\Request;
 
 class CitizenController extends Controller
 {
-    //
+    public function index()
+    {
+        $citizens = Citizen::all();
+        return response()->json($citizens);
+    }
+
+    public function show($id)
+    {
+        $citizen = Citizen::find($id);
+        return response()->json($citizen);
+    }
+
+    public function store(Request $request)
+    {
+        $citizen = Citizen::create($request->all());
+        return response()->json($citizen, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $citizen = Citizen::findOrFail($id);
+        $citizen->update($request->all());
+        return response()->json($citizen, 200);
+    }
+
+    public function delete($id)
+    {
+        Citizen::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
 }
