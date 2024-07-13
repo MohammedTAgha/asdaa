e29e9e4f84dd02eb82c050230fb4f9631f1d094b
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-07-13 10:05:50 +0300

    create child module and controller

---
+++ app/Models/Child.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Child extends Model
+{
+    // use HasFactory;
+    protected $table='childrens';
+    protected $primaryKey='id';
+    public $incrementing=false;
+
+    protected $fillable = [
+        'name',
+     'date_of_birth',
+      'gender',
+    'citizen_id',
+    'orphan',
+    'infant',
+    'bambers_size',
+    'disease',
+    'disease_description',
+    'obstruction',
+    'obstruction_description',
+    'note',
+
+    ];
+    
+    public function citizen() //father
+    {
+        return $this->belongsTo(Citizen::class);
+    }
+
+    public function age()
+    {
+        return \Carbon\Carbon::parse($this->date_of_birth)->age;
+    }
+}
