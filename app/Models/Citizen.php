c862f34d33e379dd9d1ee0f28f940487124c4946
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-07-13 10:36:16 +0300

    Update Citizen.php

--- app/Models/Citizen.php
+++ app/Models/Citizen.php
@@ -5,8 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Citizen extends Model
 {
+    protected $table='citizens';
+    protected $fillable = [
+        'id',
+        'name',
+        'date_of_birth',
+        'gender',
+        'wife_name',
+        'wife_id',
+        'widowed',
+        'social_status',
+        'living_status',
+        'job',
+        'original_address',
+        'elderly_count',
+        'region_id ',
+        'note',
+    
+        ];
+   
+    protected $primaryKey='id';
+    public $incrementing=false;
     public function region()
     {
         return $this->belongsTo(Region::class);
