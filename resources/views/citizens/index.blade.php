ea00a3a73eb74af6f766c4c58a4cdcf90fa3739c
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-07-19 19:22:00 +0300

    ..

--- resources/views/citizens/index.blade.php
+++ resources/views/citizens/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     @component('components.toolbar',['title'=>'المواطنين'])
+
             <!--begin::Compact form-->
             <form method="GET" action="{{ route('citizens.index') }}">
             <div class="d-flex align-items-center">
@@ -19,8 +20,76 @@
         </div>
         </form>
         <!--end::Compact form-->
+        @slot('side')
+            <!--begin::Menu-->
+            <a  class="btn btn-sm btn-flex btn-light btn-active-primary fw-bolder" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+            <!--begin::Svg Icon | path: icons/duotune/general/gen031.svg-->
+            <span class="svg-icon svg-icon-5 svg-icon-gray-500 me-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z" fill="black" />
+                </svg>
+            </span>
+            <!--end::Svg Icon-->فلترة</a>
+            <!--begin::Menu 1-->
+            <div class="menu menu-sub menu-sub-dropdown w-250px w-md-300px" data-kt-menu="true" id="kt_menu_61484c45b0e77">
+                <!--begin::Header-->
+                <div class="px-5 py-3">
+                    <div class="fs-6 text-dark fw-bolder">خيارات التصنيف</div>
+                </div>
+                <!--end::Header-->
+                <!--begin::Menu separator-->
+                <div class="separator border-gray-200"></div>
+                <!--end::Menu separator-->
+                <!--begin::Form-->
+                <div class="px-5 py-3">
+                    <!--begin::Input group-->
+                    <div class="mb-6">
+                        <!--begin::Label-->
+                        <label class="form-label fw-bold">Status:</label>
+                        <!--end::Label-->
+                        <!--begin::Input-->
+                        <div>
+                            <select class="form-select form-select-solid" data-kt-select2="true" data-placeholder="Select option" data-dropdown-parent="#kt_menu_61484c45b0e77" data-allow-clear="true">
+                                <option></option>
+                                <option value="1">Approved</option>
+                                <option value="2">Pending</option>
+                                <option value="2">In Process</option>
+                                <option value="2">Rejected</option>
+                            </select>
+                        </div>
+                        <!--end::Input-->
+                    </div>
+                    <!--end::Input group-->
+                    
+                    <!--begin::Input group-->
+                    <div class="mb-10">
+                        <!--begin::Label-->
+                        <label class="form-label fw-bold">Notifications:</label>
+                        <!--end::Label-->
+                        <!--begin::Switch-->
+                        <div class="form-check form-switch form-switch-sm form-check-custom form-check-solid">
+                            <input class="form-check-input" type="checkbox" value="" name="notifications" checked="checked" />
+                            <label class="form-check-label">Enabled</label>
+                        </div>
+                        <!--end::Switch-->
+                    </div>
+                    <!--end::Input group-->
+                    <!--begin::Actions-->
+                    <div class="d-flex justify-content-end">
+                        <button type="reset" class="btn btn-sm btn-light btn-active-light-primary me-2" data-kt-menu-dismiss="true">Reset</button>
+                        <button type="submit" class="btn btn-sm btn-primary" data-kt-menu-dismiss="true">Apply</button>
+                    </div>
+                    <!--end::Actions-->
+                </div>
+                <!--end::Form-->
+            </div>
+            <!--end::Menu 1-->
+            <!--end::Menu-->
+            <a href="{{ route('citizens.create') }}" class="btn btn-sm btn-primary">اضافة جديد</a>
+
+        @endslot
     @endcomponent
-    
+
 aa
         <div class="relative overflow-x-auto shadow-md px-4 sm:rounded-lg">
         <!-- <h1 class="text-2xl font-bold my-4">المواطنين</h1>
