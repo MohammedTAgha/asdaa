688074e477397b07ee9edbc631d4a6c1261e8b67
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-07-20 17:55:34 +0300

    update show style

--- resources/views/distributions/show.blade.php
+++ resources/views/distributions/show.blade.php
@@ -1,24 +1,73 @@
 @extends('dashboard')
 
 @section('content')
-<div class="container mx-auto px-4">
-    <h1 class="text-2xl font-bold my-4">{{ $distribution->name }}</h1>
-    <p>Date: {{ $distribution->date }}</p>
-    <p>Arrive Date: {{ $distribution->arrive_date }}</p>
-    <p>Quantity: {{ $distribution->quantity }}</p>
-    <p>Target: {{ $distribution->target }}</p>
-    <p>Source: {{ $distribution->source }}</p>
-    <p>Done: {{ $distribution->done ? 'Yes' : 'No' }}</p>
-    <p>Target Count: {{ $distribution->target_count }}</p>
-    <p>Expectation: {{ $distribution->expectation }}</p>
-    <p>Min Count: {{ $distribution->min_count }}</p>
-    <p>Max Count: {{ $distribution->max_count }}</p>
-    <p>Note: {{ $distribution->note }}</p>
-    <ul>
-    
+
+@component('components.box',['title'=>'بيانات التوزيع'])
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+                <label class="block text-sm font-medium text-gray-700">رقم</label>
+                <p class="mt-1 text-gray-900">{{ $distribution->id }}</p>
+            </div>
+            
+            <div>
+                <label class="block text-sm font-medium text-gray-700">الوصف</label>
+                <p class="mt-1 text-gray-900">{{ $distribution->name }}</p>
+            </div>
+            
+            <div>
+                <label class="block text-sm font-medium text-gray-700">تاريخ الوصول</label>
+                <p class="mt-1 text-gray-900">{{ $distribution->arrive_date }}</p>
+            </div>
+            
+            <div>
+                <label class="block text-sm font-medium text-gray-700">الكمية</label>
+                <p class="mt-1 text-gray-900">{{ $distribution->quantity }}</p>
+            </div>
+            
+            <div>
+                <label class="block text-sm font-medium text-gray-700">الفئة المستهدفة</label>
+                <p class="mt-1 text-gray-900">{{ $distribution->target }}</p>
+            </div>
+            
+            <div>
+                <label class="block text-sm font-medium text-gray-700">المصدر</label>
+                <p class="mt-1 text-gray-900">{{ $distribution->source }}</p>
+            </div>
+            
+            <div>
+                <label class="block text-sm font-medium text-gray-700">اكتمل</label>
+                <p class="mt-1 text-gray-900">{{ $distribution->done ? 'مكتمل' : 'غير مكتمل' }}</p>
+            </div>
+            
+            <div>
+                <label class="block text-sm font-medium text-gray-700">عدد المستفيدين</label>
+                <p class="mt-1 text-gray-900">{{ $distribution->target_count }}</p>
+            </div>
+            
+            <div>
+                <label class="block text-sm font-medium text-gray-700">المتوقعين</label>
+                <p class="mt-1 text-gray-900">{{ $distribution->expectation }}</p>
+            </div>
+            
+            <div>
+                <label class="block text-sm font-medium text-gray-700">عدد الافراد</label>
+                <p class="mt-1 text-gray-900">من : {{ $distribution->min_count }} {{ $distribution->max_count }}الى:</p>
+            </div>
+            
+            <div>
+                <label class="block text-sm font-medium text-gray-700">ملاحظة</label>
+                <p class="mt-1 text-gray-900">{{ $distribution->note }}</p>
+            </div>
+
+        </div>
     @foreach($distribution->citizens as $citizen)
     <li> {{$citizen->id }}  | {{$citizen->name }}  </li>
     @endforeach
     </ul>
 </div>
+@endcomponent
+
+@component('components.box',['title'=>'المواطنين المستفيدين])
+    
+@endcomponent
 @endsection
