563928704c11292cecef3cf69ddcccbed6596c00
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-07-23 17:22:19 +0300

    add the new table layout for ctz
    from metronic costumers page

--- resources/views/components/datatable.blade.php
+++ resources/views/components/datatable.blade.php
@@ -1,65 +1,18 @@
 <div class="card card-flush mt-6 mt-xl-9">
     <!--begin::Card header-->
-    <div class="card-header mt-5">
-        <!--begin::Card title-->
-        <div class="card-title flex-column">
-            <h3 class="fw-bolder mb-1">Project Spendings</h3>
-            <div class="fs-6 text-gray-400">Total $260,300 sepnt so far</div>
-        </div>
-        <!--begin::Card title-->
-        <!--begin::Card toolbar-->
-        <div class="card-toolbar my-1">
-            <!--begin::Select-->
-            <div class="me-6 my-1">
-                <select id="kt_filter_year" name="year" data-control="select2" data-hide-search="true" class="w-125px form-select form-select-solid form-select-sm">
-                    <option value="All" selected="selected">All time</option>
-                    <option value="thisyear">This year</option>
-                    <option value="thismonth">This month</option>
-                    <option value="lastmonth">Last month</option>
-                    <option value="last90days">Last 90 days</option>
-                </select>
-            </div>
-            <!--end::Select-->
-            <!--begin::Select-->
-            <div class="me-4 my-1">
-                <select id="kt_filter_orders" name="orders" data-control="select2" data-hide-search="true" class="w-125px form-select form-select-solid form-select-sm">
-                    <option value="All" selected="selected">All Orders</option>
-                    <option value="Approved">Approved</option>
-                    <option value="Declined">Declined</option>
-                    <option value="In Progress">In Progress</option>
-                    <option value="In Transit">In Transit</option>
-                </select>
-            </div>
-            <!--end::Select-->
-            <!--begin::Search-->
-            <div class="d-flex align-items-center position-relative my-1">
-                <!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
-                <span class="svg-icon svg-icon-3 position-absolute ms-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="black" />
-                        <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="black" />
-                    </svg>
-                </span>
-                <!--end::Svg Icon-->
-                <input type="text" id="kt_filter_search" class="form-control form-control-solid form-select-sm w-150px ps-9" placeholder="Search Order" />
-            </div>
-            <!--end::Search-->
-        </div>
-        <!--begin::Card toolbar-->
-    </div>
     <!--end::Card header-->
     <!--begin::Card body-->
     <div class="card-body pt-0">
         <!--begin::Table container-->
         <div class="table-responsive">
             <!--begin::Table-->
-            <table id="kt_profile_overview_table" class="table table-row-bordered table-row-dashed gy-4 align-middle fw-bolder">
-                <!--begin::Head-->
+            <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_customers_table">
+            <!--begin::Head-->
                 <thead class="fs-7 text-gray-400 text-uppercase">
-                    <tr>
+                <tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
                         <th class="w-15px">
                             <div class="form-check form-check-sm form-check-custom form-check-solid">
-                                <input class="form-check-input" type="checkbox" value="1" data-kt-check="true" data-kt-check-target=".widget-13-check" />
+                                <input class="form-check-input" type="checkbox" value="1" data-kt-check="true" data-kt-check-target="#kt_customers_table .form-check-input" />
                             </div>
                         </th>
                         <th class="min-w-90px">الهوية</th>
@@ -75,14 +28,16 @@
                 </thead>
                 <!--end::Head-->
                 <!--begin::Body-->
-                <tbody class="fs-6">
+                <tbody class="fw-bold text-gray-600">
                 @foreach ($citizens as $citizen)
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <!--begin::Checkbox-->
                         <td class="px-2 py-1">
-                        <div class="form-check form-check-sm form-check-custom form-check-solid">
-                            <input class="form-check-input widget-13-check" type="checkbox" value="1" />
-                        </div>
+                            <div class="form-check form-check-sm form-check-custom form-check-solid">
+                                <input class="form-check-input" type="checkbox" value="1" />
+                            </div>
                         </td>
+                        <!--begin::Checkbox-->
                         <td class="px-2 py-1 bg-gray-50">
                         <a href="{{ route('citizens.show',$citizen->id) }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700 hover:text-white">
                             {{ $citizen->id }}
@@ -118,11 +73,14 @@
 <script src="assets/plugins/custom/datatables/datatables.bundle.js"></script>
 <!--end::Page Vendors Javascript-->
 <!--begin::Page Custom Javascript(used by this page)-->
-<script src="assets/js/custom/pages/projects/project/project.js"></script>
-<script src="assets/js/custom/modals/users-search.js"></script>
-<script src="assets/js/custom/modals/new-target.js"></script>
+<script src="assets/js/custom/apps/customers/list/export.js"></script>
+<script src="assets/js/custom/apps/customers/list/list.js"></script>
+<script src="assets/js/custom/apps/customers/add.js"></script>
 <script src="assets/js/custom/widgets.js"></script>
 <script src="assets/js/custom/apps/chat/chat.js"></script>
 <script src="assets/js/custom/modals/create-app.js"></script>
 <script src="assets/js/custom/modals/upgrade-plan.js"></script>
+<!-- <script src="assets/js/custom/apps/chat/chat.js"></script> -->
+<!-- <script src="assets/js/custom/modals/create-app.js"></script> -->
+<!-- <script src="assets/js/custom/modals/upgrade-plan.js"></script> -->
 <!--end::Page Custom Javascript-->
