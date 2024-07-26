6ec610ab35df6859566c9b7e9bb8f6f98464f6c6
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-07-26 14:13:39 +0300

    format

--- app/Http/Controllers/DistributionController.php
+++ app/Http/Controllers/DistributionController.php
@@ -16,183 +16,223 @@ class DistributionController extends Controller
 {
     public function index()
     {
-        $distributions = Distribution::with('category')->get();
-        return view('distributions.index', compact('distributions'));
+        $distributions = Distribution::with("category")->get();
+        return view("distributions.index", compact("distributions"));
     }
 
     public function create()
     {
         $categories = DistributionCategory::all();
-        return view('distributions.create', compact('categories'));
+        return view("distributions.create", compact("categories"));
     }
 
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required',
-            'date' => 'required|date',
-            'distribution_category_id' => 'required|exists:distribution_categories,id',
-            'arrive_date' => 'required|date',
-            'quantity' => 'required|integer',
-            'target' => 'required',
-            'source' => 'required',
-            'done' => 'required|boolean',
-            'target_count' => 'required|integer',
-            'expectation' => 'required',
-            'min_count' => 'required|integer',
-            'max_count' => 'required|integer',
-            'note' => 'nullable|string',
+            "name" => "required",
+            "date" => "required|date",
+            "distribution_category_id" =>
+                "required|exists:distribution_categories,id",
+            "arrive_date" => "required|date",
+            "quantity" => "required|integer",
+            "target" => "required",
+            "source" => "required",
+            "done" => "required|boolean",
+            "target_count" => "required|integer",
+            "expectation" => "required",
+            "min_count" => "required|integer",
+            "max_count" => "required|integer",
+            "note" => "nullable|string",
         ]);
 
         Distribution::create($request->all());
 
-        return redirect()->route('distributions.index')->with('success', 'Distribution created successfully.');
+        return redirect()
+            ->route("distributions.index")
+            ->with("success", "Distribution created successfully.");
     }
 
     public function show($id)
     {
         $distribution = Distribution::findOrFail($id);
-        return view('distributions.show', compact('distribution'));
+        return view("distributions.show", compact("distribution"));
     }
 
     public function edit(Distribution $distribution)
     {
         $categories = DistributionCategory::all();
-        return view('distributions.edit', compact('distribution', 'categories'));
+        return view(
+            "distributions.edit",
+            compact("distribution", "categories")
+        );
     }
 
     public function update(Request $request, Distribution $distribution)
     {
         $request->validate([
-            'name' => 'required',
-            'date' => 'required|date',
-            'distribution_category_id' => 'required|exists:distribution_categories,id',
-            'arrive_date' => 'required|date',
-            'quantity' => 'required|integer',
-            'target' => 'required',
-            'source' => 'required',
-            'done' => 'required|boolean',
-            'target_count' => 'required|integer',
-            'expectation' => 'required',
-            'min_count' => 'required|integer',
-            'max_count' => 'required|integer',
-            'note' => 'nullable|string',
+            "name" => "required",
+            "date" => "required|date",
+            "distribution_category_id" =>
+                "required|exists:distribution_categories,id",
+            "arrive_date" => "required|date",
+            "quantity" => "required|integer",
+            "target" => "required",
+            "source" => "required",
+            "done" => "required|boolean",
+            "target_count" => "required|integer",
+            "expectation" => "required",
+            "min_count" => "required|integer",
+            "max_count" => "required|integer",
+            "note" => "nullable|string",
         ]);
 
         $distribution->update($request->all());
 
-        return redirect()->route('distributions.index')->with('success', 'Distribution updated successfully.');
+        return redirect()
+            ->route("distributions.index")
+            ->with("success", "Distribution updated successfully.");
     }
 
     public function updatePivot(Request $request)
-{
-    $pivotId = $request->input('pivotId');
-    $isChecked = $request->input('isChecked');
-    $selectedDate = $request->input('selectedDate');
-    $quantity = $request->input('quantity');
-    $recipient = $request->input('recipient');
-    $note = $request->input('note');
-
-    $data=0;
-    if ($isChecked ===true){
-        $data = 1;
-    }
-    
-    // Retrieve the pivot record and update the "done" state
-    $pivot = DistributionCitizen::find($pivotId);
-    $pivot->done = $isChecked;
-    $pivot->date=$selectedDate;
-     $pivot->quantity=$quantity;
-     $pivot->recipient=$recipient;
-     $pivot->note=$note;
-     
-    $pivot->save();
-
-    return response()->json(['message' => $request->input('selectedDate')]);
-}
-public function getDistributions()
-{
-    $distributions = Distribution::all();
-    return response()->json(['distributions' => $distributions]);
-}
-public function addCitizens(Request $request, $distributionId = null)
     {
-        $citizenIds = explode(',', $request->input('citizen_ids'));
-    $distributionId = $distributionId ?? $request->input('distribution_id');
-    $truncatedCitizens = [];
-    $existingCitizenNames = [];
+        $pivotId = $request->input("pivotId");
+        $isChecked = $request->input("isChecked");
+        $selectedDate = $request->input("selectedDate");
+        $quantity = $request->input("quantity");
+        $recipient = $request->input("recipient");
+        $note = $request->input("note");
 
-    DB::beginTransaction();
-    try {
-        Log::error('Citizen IDs:', ['ids' => $citizenIds]);
+        $data = 0;
+        if ($isChecked === true) {
+            $data = 1;
+        }
 
-        // Retrieve existing citizens in the distribution
-        $existingCitizens = DB::table('distribution_citizens')
-            ->where('distribution_id', $distributionId)
-            ->whereIn('citizen_id', $citizenIds)
-            ->pluck('citizen_id')
-            ->toArray();
+        // Retrieve the pivot record and update the "done" state
+        $pivot = DistributionCitizen::find($pivotId);
+        $pivot->done = $isChecked;
+        $pivot->date = $selectedDate;
+        $pivot->quantity = $quantity;
+        $pivot->recipient = $recipient;
+        $pivot->note = $note;
 
-        // Filter out existing citizens
-        $newCitizenIds = array_diff($citizenIds, $existingCitizens);
+        $pivot->save();
 
-        foreach ($newCitizenIds as $citizenId) {
-            Log::error('Citizen ID:', ['...' => $citizenId]);
-            try {
-                DB::table('distribution_citizens')->insert([
-                    'distribution_id' => $distributionId,
-                    'citizen_id' => $citizenId,
-                ]);
-            } catch (QueryException $e) {
-                if (strpos($e->getMessage(), 'Data truncated') !== false) {
-                    $truncatedCitizens[] = $citizenId;
-                } else {
-                    throw $e;
+        return response()->json(["message" => $request->input("selectedDate")]);
+    }
+    public function getDistributions()
+    {
+        $distributions = Distribution::all();
+        return response()->json(["distributions" => $distributions]);
+    }
+    public function addCitizens(Request $request, $distributionId = null)
+    {
+        $citizenIds = explode(",", $request->input("citizen_ids"));
+        $distributionId = $distributionId ?? $request->input("distribution_id");
+        $truncatedCitizens = [];
+        $existingCitizenNames = [];
+
+        DB::beginTransaction();
+        try {
+            Log::error("Citizen IDs:", ["ids" => $citizenIds]);
+
+            // Retrieve existing citizens in the distribution
+            $existingCitizens = DB::table("distribution_citizens")
+                ->where("distribution_id", $distributionId)
+                ->whereIn("citizen_id", $citizenIds)
+                ->pluck("citizen_id")
+                ->toArray();
+
+            // Filter out existing citizens
+            $newCitizenIds = array_diff($citizenIds, $existingCitizens);
+
+            foreach ($newCitizenIds as $citizenId) {
+                Log::error("Citizen ID:", ["..." => $citizenId]);
+                try {
+                    DB::table("distribution_citizens")->insert([
+                        "distribution_id" => $distributionId,
+                        "citizen_id" => $citizenId,
+                    ]);
+                } catch (QueryException $e) {
+                    if (strpos($e->getMessage(), "Data truncated") !== false) {
+                        $truncatedCitizens[] = $citizenId;
+                    } else {
+                        throw $e;
+                    }
                 }
+            }
+
+            // Get names of existing citizens
+            if (!empty($existingCitizens)) {
+                $existingCitizenNames = Citizen::whereIn(
+                    "id",
+                    $existingCitizens
+                )
+                    ->pluck("name")
+                    ->toArray();
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error("Error adding citizens: ", [
+                "error" => $e->getMessage(),
+            ]);
+            if ($request->expectsJson()) {
+                return response()->json(
+                    ["error" => "An error occurred while adding citizens."],
+                    500
+                );
+            } else {
+                return redirect()
+                    ->back()
+                    ->with("error", "An error occurred while adding citizens.");
             }
         }
 
-        // Get names of existing citizens
-        if (!empty($existingCitizens)) {
-            $existingCitizenNames = Citizen::whereIn('id', $existingCitizens)->pluck('name')->toArray();
+        if (!empty($truncatedCitizens)) {
+            if ($request->expectsJson()) {
+                return response()->json(
+                    ["truncated_citizens" => $truncatedCitizens],
+                    400
+                );
+            } else {
+                return redirect()
+                    ->back()
+                    ->with(
+                        "error",
+                        "Some citizens could not be added due to data truncation."
+                    );
+            }
         }
 
-        DB::commit();
-    } catch (\Exception $e) {
-        DB::rollBack();
-        Log::error('Error adding citizens: ', ['error' => $e->getMessage()]);
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'An error occurred while adding citizens.'], 500);
-        } else {
-            return redirect()->back()->with('error', 'An error occurred while adding citizens.');
+        if (!empty($existingCitizenNames)) {
+            if ($request->expectsJson()) {
+                return response()->json(
+                    ["existing_citizens" => $existingCitizenNames],
+                    200
+                );
+            } else {
+                return redirect()
+                    ->back()
+                    ->with(
+                        "warning",
+                        "Some citizens already exist in the distribution."
+                    );
+            }
+
+            return response()->json(
+                ["success" => "Citizens added successfully."],
+                200
+            );
         }
-        }
-
-    if (!empty($truncatedCitizens)) {
-        if ($request->expectsJson()) {
-            return response()->json(['truncated_citizens' => $truncatedCitizens], 400);
-        } else {
-            return redirect()->back()->with('error', 'Some citizens could not be added due to data truncation.');
-        }    }
-
-    if (!empty($existingCitizenNames)) {
-        if ($request->expectsJson()) {
-            return response()->json(['existing_citizens' => $existingCitizenNames], 200);
-        } else {
-            return redirect()->back()->with('warning', 'Some citizens already exist in the distribution.');    }
-
-    if ($request->expectsJson()) {
-        return response()->json(['success' => 'Citizens added successfully.'], 200);
-    } else {
-        return redirect()->back()->with('success', 'Citizens added successfully.');
-    }
-    }
     }
 
     public function destroy(Distribution $distribution)
     {
         $distribution->delete();
 
-        return redirect()->route('distributions.index')->with('success', 'Distribution deleted successfully.');
+        return redirect()
+            ->route("distributions.index")
+            ->with("success", "Distribution deleted successfully.");
     }
 }
