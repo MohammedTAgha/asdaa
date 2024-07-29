e344ae5f72869a7ca730d83b67c01317fed21f34
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-07-29 19:58:04 +0300

    delete it

--- resources/views/citizens/index.blade.php
+++ resources/views/citizens/index.blade.php
@@ -5,49 +5,7 @@
 
 @component('components.toolbar',['title'=>'المواطنين'])
 
-<form action="#" data-select2-id="select2-data-86-t9rz">
-    <!--begin::Card-->
-    <div class="card mb-7" data-select2-id="select2-data-85-v0vm">
-        <!--begin::Card body-->
-        <div class="card-body" data-select2-id="select2-data-84-b7f1">
-            <!--begin::Compact form-->
-            <div class="d-flex align-items-center">
-                <!--begin::Input group-->
-                <div class="position-relative w-md-400px me-md-2">
-                    <!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
-                    <span class="svg-icon svg-icon-3 svg-icon-gray-500 position-absolute top-50 translate-middle ms-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="black"></rect>
-                            <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="black"></path>
-                        </svg>
-                    </span>
-                    <!--end::Svg Icon-->
-                    <input type="text" class="form-control form-control-solid ps-10" name="search" value="" placeholder="Search">
-                </div>
-                <!--end::Input group-->
-                <!--begin:Action-->
-                <div class="d-flex align-items-center">
-                    <button type="submit" class="btn btn-primary me-5">Search</button>
-                    <a id="kt_horizontal_search_advanced_link" class="btn btn-link collapsed" data-bs-toggle="collapse" href="#kt_advanced_search_form" aria-expanded="false">Advanced Search</a>
-                </div>
-                <!--end:Action-->
-            </div>
-            <!--end::Compact form-->
-            <!--begin::Advance form-->
-            <div class="collapse" id="kt_advanced_search_form" style="" data-select2-id="select2-data-kt_advanced_search_form">
-                <!--begin::Separator-->
-                <div class="separator separator-dashed mt-9 mb-6"></div>
-                <!--end::Separator-->
-                <!--begin::Row-->
 
-                <!--end::Row-->
-            </div>
-            <!--end::Advance form-->
-        </div>
-        <!--end::Card body-->
-    </div>
-    <!--end::Card-->
-</form>
     @slot('side')
     
     @endslot
