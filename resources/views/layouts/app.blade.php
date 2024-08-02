90cccdf4e7691808ae72ce581d9133f2b3cd45d5
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-08-02 15:32:22 +0300

    update reion show

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -281,6 +281,7 @@
                     <div class="container-xxl flex-grow-1 container-p-y">
 
                         <h4 class="fw-bold py-3 mb-4">Page title</h4>
+                        
                         @yield('content')
                         <!-- Snackbar container -->
                         <div id="snackbar" class="snackbar"></div>
--- resources/views/regions/show.blade.php
+++ resources/views/regions/show.blade.php
@@ -1,12 +1,26 @@
 @extends('dashboard')
 
 @section('content')
-    <div class="container mx-auto py-12">
-        <h1 class="text-4xl font-bold mb-4">بيانات المنطقة</h1>
+@component('components.box', ['title' => 'منطقة' . ' ' . $region->name . ' ' , 'styles'=> 'mb-4'])
+
+    
+        {{-- <h1 class="text-4xl font-bold mb-4">بيانات المنطقة</h1> --}}
         <div class="bg-white shadow-md rounded-lg p-6">
             <div class="mb-4">
                 <h2 class="text-2xl font-semibold">الاسم:</h2>
                 <p class="text-gray-700">{{ $region->name }}</p>
+            </div>
+            <div class="mb-4">
+                <h2 class="text-2xl font-semibold">مندوبها:</h2>
+                
+                @if (!$region->representatives->isEmpty())
+                    @foreach ($region->representatives as $representative)
+                    <p class="text-gray-700">{{ $representative->name }} </p>
+                    @endforeach
+                @else
+                <p class="text-gray-700">N/A</p>
+                @endif
+                
             </div>
             <div class="mb-4">
                 <h2 class="text-2xl font-semibold">الموقع:</h2>
@@ -20,4 +34,10 @@
                 <a href="{{ route('regions.index') }}" class="bg-gray-500 text-white px-4 py-2 rounded">رجوع</a>
             </div>
         </div>
-    </div>
+
+    @endcomponent
+@component('components.box', ['title' => ' المواطنين في منطقة' . ' ' . $region->name , 'styles'=> 'mb-8'])
+            <x-citizens :citizens="$region->citizens" />
+@endcomponent
+
+@endsection
