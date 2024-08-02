569f5e0cff2b1884aac8e5f8b02739adca5e4430
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-08-02 12:42:43 +0300

    add {{ asset( to tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,28 +26,28 @@
     rel="stylesheet" />
 
   <!-- Icons -->
-  <link rel="stylesheet" href="../../assets/vendor/fonts/fontawesome.css" />
-  <link rel="stylesheet" href="../../assets/vendor/fonts/tabler-icons.css" />
-  <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/fontawesome.css')}}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/tabler-icons.css')}}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/flag-icons.css')}}" />
 
   <!-- Core CSS -->
-  <link rel="stylesheet" href="../../assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
-  <link rel="stylesheet" href="../../assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
-  <link rel="stylesheet" href="../../assets/css/demo.css" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/core.css')}}" class="template-customizer-core-css" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/theme-default.css')}}" class="template-customizer-theme-css" />
+  <link rel="stylesheet" href="{{ asset('assets/css/demo.css')}}" />
 
   <!-- Vendors CSS -->
-  <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-  <link rel="stylesheet" href="../../assets/vendor/libs/node-waves/node-waves.css" />
-  <link rel="stylesheet" href="../../assets/vendor/libs/typeahead-js/typeahead.css" />
-  <link rel="stylesheet" href="../../assets/vendor/libs/bs-stepper/bs-stepper.css" />
-  <link rel="stylesheet" href="../../assets/vendor/libs/bootstrap-select/bootstrap-select.css" />
-  <link rel="stylesheet" href="../../assets/vendor/libs/select2/select2.css" />
-  <link rel="stylesheet" href="../../assets/vendor/libs/formvalidation/dist/css/formValidation.min.css" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/node-waves/node-waves.css')}}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/typeahead-js/typeahead.css')}}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bs-stepper/bs-stepper.css')}}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap-select/bootstrap-select.css')}}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css')}}" />
+  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/formvalidation/dist/css/formValidation.min.css')}}" />
 
   <!-- Page CSS -->
   @yield('styles')
   <!-- Helpers -->
-  <script src="../../assets/vendor/js/helpers.js"></script>
+  <script src="{{ asset('assets/vendor/js/helpers.js')}}"></script>
 
   <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
   <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
@@ -314,34 +314,34 @@
   
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
-    <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="../../assets/vendor/libs/popper/popper.js"></script>
-    <script src="../../assets/vendor/js/bootstrap.js"></script>
-    <script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="../../assets/vendor/libs/node-waves/node-waves.js"></script>
+    <script src="{{ asset('assets/vendor/libs/jquery/jquery.js')}}"></script>
+    <script src="{{ asset('assets/vendor/libs/popper/popper.js')}}"></script>
+    <script src="{{ asset('assets/vendor/js/bootstrap.js')}}"></script>
+    <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+    <script src="{{ asset('assets/vendor/libs/node-waves/node-waves.js')}}"></script>
   
-    <script src="../../assets/vendor/libs/hammer/hammer.js"></script>
-    <script src="../../assets/vendor/libs/i18n/i18n.js"></script>
-    <script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
+    <script src="{{ asset('assets/vendor/libs/hammer/hammer.js')}}"></script>
+    <script src="{{ asset('assets/vendor/libs/i18n/i18n.js')}}"></script>
+    <script src="{{ asset('assets/vendor/libs/typeahead-js/typeahead.js')}}"></script>
   
-    <script src="../../assets/vendor/js/menu.js"></script>
+    <script src="{{ asset('assets/vendor/js/menu.js')}}"></script>
     <!-- endbuild -->
   
     <!-- Vendors JS -->
-    <script src="../../assets/vendor/libs/bs-stepper/bs-stepper.js"></script>
-    <script src="../../assets/vendor/libs/bootstrap-select/bootstrap-select.js"></script>
-    <script src="../../assets/vendor/libs/select2/select2.js"></script>
-    <script src="../../assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js"></script>
-    <script src="../../assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js"></script>
-    <script src="../../assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js"></script>
+    <script src="{{ asset('assets/vendor/libs/bs-stepper/bs-stepper.js')}}"></script>
+    <script src="{{ asset('assets/vendor/libs/bootstrap-select/bootstrap-select.js')}}"></script>
+    <script src="{{ asset('assets/vendor/libs/select2/select2.js')}}"></script>
+    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js')}}"></script>
+    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js')}}"></script>
+    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js')}}"></script>
   
     <!-- Main JS -->
-    <script src="../../assets/js/main.js"></script>
+    <script src="{{ asset('assets/js/main.js')}}"></script>
   
     <!-- Page JS -->
     @stack('scripts')
-    <script src="../../assets/js/form-wizard-numbered.js"></script>
-    <script src="../../assets/js/form-wizard-validation.js"></script>
+    <script src="{{ asset('assets/js/form-wizard-numbered.js')}}"></script>
+    <script src="{{ asset('assets/js/form-wizard-validation.js')}}"></script>
   
   </body>
 
