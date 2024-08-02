1b08948e6fbb6b0ec17d63f02c58e6bcf8e64940
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-08-02 22:32:41 +0300

    regionnew desih

--- resources/views/regions/index.blade.php
+++ resources/views/regions/index.blade.php
@@ -1,10 +1,42 @@
 @extends('dashboard')
 
 @section('content')
-    <div class="container mx-auto py-12">
-        <h1 class="text-4xl font-bold mb-4">المناطق</h1>
-        <a href="{{ route('regions.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded">Create New Region</a>
-        <div class="mt-6">
+    <div class="container mx-auto py-6">
+        <div class="card mb-4">
+            <div class="card-header header-elements">
+                <h2 class="text-2xl font-bold ">المناطق</h1>
+                    <span class="h-13px border-gray-200 border-start mx-2 mx-2"></span>
+
+                    <a href="{{ route('regions.create') }}" class="bg-blue-500 text-white px-2 py-1 rounded">جديد</a>
+
+                    <div class="card-header-elements ms-auto d-flex">
+                        <input type="text" class="form-control form-control-sm" placeholder="Search">
+
+                    </div>
+
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    @foreach ($regions as $region)
+                   
+                    <div class="col-md-6 col-xl-4 bg-primary px-0 rounded" style="max-width: 32%;margin: 2px ; ">
+                        <a href="{{route('regions.show',$region->id)}}">
+                        <div class="card bg-primary text-white mb-0 px-0 rounded">
+                            <div class="card-header text-xl font-bold py-3"> {{$region->name}} </div>
+                            <div class="card-body text-xl mb-0 pb-0">
+                                <h5 class=" text-white"> {{$region->representatives->first()->name ?? 'غير محدد'}} </h5>
+                            </div>                       
+                        </div>
+                        <div class=" text-4xl font-bold text-center mt-1 mb-2 text-white">{{$region->citizens->count() ?? '  --  '}} <span class="fs-3">اسرة</span> </div>
+                            </a>
+                    </div>
+                    
+                    @endforeach
+
+
+                </div>
+            </div>
+            {{-- <div class="mt-6">
             <table class="min-w-full bg-white">
                 <thead class="bg-gray-800 text-white">
                     <tr>
@@ -42,6 +74,6 @@
                     @endforeach
                 </tbody>
             </table>
+        </div> --}}
         </div>
-    </div>
-@endsection
+    @endsection
