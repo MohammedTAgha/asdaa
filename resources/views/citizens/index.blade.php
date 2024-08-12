996437259977c7eb3206003cc9b50d211869184f
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-08-12 18:25:07 +0300

    remove top citizens bar from ctz index

--- resources/views/citizens/index.blade.php
+++ resources/views/citizens/index.blade.php
@@ -171,160 +171,12 @@
             </script>
         @endif
         <div class="card px-4">
-            <div class="card-header d-flex justify-content-between">
-                <div>
-  
-                    <div class="ml-4 relative d-flex">
-                        <form method="GET" action="{{ route('citizens.index') }}" class="me-4">
-                            <div class="flex items-center w-full mx-2 me-6">
-                                <div class="col-md-6">
-                                    <input type="text" id="search" name="search" class="form-control"
-                                        placeholder="بحث عام...">
-                                </div>
-                                <button type="submit" class="ms-3 btn btn-primary waves-effect waves-light">
-                                    بحث
-                                    <span class="ti-xs ti ti-user-search ms-1"></span>
-                                </button>
 
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="me-2 relative flex">
-                    <button id="filterButton" type="button" class=" mx-1 btn btn-outline-primary waves-effect">
-                        فلترة
-                        <span class="ti-xs ti ti-filter-off ms-1"></span>
-                    </button>
-                    {{-- form --}}
-                    <form action="{{ route('citizens.export') }}" method="GET">
-                        <!-- Add hidden inputs for each filter parameter -->
-                        <input type="hidden" name="id" value="{{ request('id') }}">
-                        <input type="hidden" name="first_name" value="{{ request('first_name') }}">
-                        <input type="hidden" name="second_name" value="{{ request('second_name') }}">
-                        <input type="hidden" name="third_name" value="{{ request('third_name') }}">
-                        <input type="hidden" name="last_name" value="{{ request('last_name') }}">
-                        <input type="hidden" name="search" value="{{ request('search') }}">
-                        <input type="hidden" name="gender" value="{{ request('gender') }}">
-                        @if(is_array(request('regions')))   
-                        @foreach(request('regions') as $region)
-                            <input type="hidden" name="regions[]" value="{{ $region }}">
-                        @endforeach
-                         @else
-                            <input type="hidden" name="regions" value="{{ request('regions') }}">
-                        @endif                        <button type="submit" class=" mx-1 btn btn-success waves-effect waves-light">
-                            تصدير
-                            <span class="ti-xs ti ti-table-export ms-1"></span>
-                        </button>
-                    </form>
-                    
-
-                    <a href="{{ route('citizens.create') }}" type="button"
-                        class="btn mx-1 btn-primary waves-effect waves-light text-white">
-                        اضافة جديد
-                        <span class="ti-xs ti ti-user-plus ms-1"></span>
-                    </a>
-                    <!-- Filter Popup Menu -->
-                    <div id="filterMenu"
-                        class="absolute left-0  z-10 hidden w-80 p-2 pt-1 mt-1 bg-white border border-gray-200 rounded-lg shadow-lg">
-                        <!-- Header -->
-                        <div class="pb-1 mb-1 border-b border-gray-200">
-                            <div class="text-lg font-semibold text-gray-700">خيارات التصنيف</div>
-                        </div>
-                        <!-- Filter Form -->
-                        <form action="{{ route('citizens.index') }}" method="GET">
-                            <!-- Prepositives -->
-                            <div class="mb-4">
-                                <label class="block mb-1 font-medium text-gray-700">اختر المناديب:</label>
-                                <select id="regions" name="regions[]"
-                                    class="select2-multiple p-2  border border-gray-300 rounded-lg" style="width: 260px;"
-                                    multiple>
-                                    @foreach ($regions as $region)
-                                        <option class=" w-120px" value="{{ $region->id }}" style="width: 260px;"
-                                            {{ in_array($region, request('regions', [])) ? 'selected' : '' }}>
-
-                                            @if ($region->representatives->isNotEmpty())
-                                                {{ $region->name }} </br> :
-                                                {{ $region->representatives->first()->name }}
-                                            @else
-                                                {{ $region->name }}
-                                            @endif
-
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="ageRange" class="form-label">افراد الاسرة</label>
-                                <div class="input-group">
-                                    <input type="number" id="minAge" class="form-control" placeholder="Min">
-                                    <span class="input-group-text">-</span>
-                                    <input type="number" id="maxAge" class="form-control" placeholder="Max">
-                                </div>
-                            </div>
-                            <div class="mb-3">
-                                <label for="ageRange" class="form-label">العمر</label>
-                                <div class="input-group">
-                                    <input type="number" id="minAge" class="form-control" placeholder="Min">
-                                    <span class="input-group-text">-</span>
-                                    <input type="number" id="maxAge" class="form-control" placeholder="Max">
-                                </div>
-                            </div>
-                            <!-- Living Status -->
-                            <div class="mb-4">
-                                <label class="block mb-1 font-medium text-gray-700">حالة السكن ل:</label>
-                                <select id="living_status" name="living_status"
-                                    class="w-full p-2 border border-gray-300 rounded-lg">
-                                    <option value="">غير محدد</option>
-                                    <option value="1">سيئ</option>
-                                    <option value="2">جيد</option>
-                                    <option value="3">ممتاز</option>
-                                </select>
-                            </div>
-                            <!-- Social Status -->
-                            <div class="mb-4">
-                                <label class="block mb-1 font-medium text-gray-700">الحالة الاجنماعية :</label>
-                                <select id="social_status" name="social_status"
-                                    class="w-full p-2 border border-gray-300 rounded-lg">
-                                    <option value="">غير محدد</option>
-                                    <option value="0">اعزب</option>
-                                    <option value="1">متزوج</option>
-                                    <option value="2">ارمل</option>
-                                    <option value="3">متعدد</option>
-                                    <option value="4">مطلق</option>
-                                    <option value="5">زوجة 1</option>
-                                    <option value="6">زوجة 2</option>
-                                    <option value="7">زوجة 3</option>
-                                    <option value="8">زوجة 4</option>
-
-                                </select>
-                            </div>
-                            <!-- Gender -->
-                            <div class="mb-4">
-                                <label class="block mb-1 font-medium text-gray-700">الجنس:</label>
-                                <select id="gender" name= "gender"
-                                    class="w-full p-2 border border-gray-300 rounded-lg">
-                                    <option value="">غير محدد</option>
-                                    <option value="0">ذكر</option>
-                                    <option value="1">انثى</option>
-                                </select>
-                            </div>
-                            <!-- Actions -->
-                            <div class="flex justify-end">
-                                <button id="close" type="button"
-                                    class="px-4 py-2 mr-2 text-gray-700 bg-gray-100 border border-gray-300 rounded-lg hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500">
-                                    اغلاق</button>
-                                <button id="applyFilters" type="submit"
-                                    class="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">تطبيق</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
             @component('components.citizens', [
                 'citizens' => $citizens,
                 'distributions' => $distributions,
                 'distributionId' => $distributionId ? $distributionId : null,
+                'regions'=>$regions,
             ])
             @endcomponent
 
@@ -398,51 +250,51 @@
         });
         document.addEventListener('DOMContentLoaded', function() {
             // Toggle filter menu
-            const filterButton = document.getElementById('filterButton');
-            const closeButton = document.getElementById('close');
-            const filterMenu = document.getElementById('filterMenu');
+            // const filterButton = document.getElementById('filterButton');
+            // const closeButton = document.getElementById('close');
+            // const filterMenu = document.getElementById('filterMenu');
 
-            filterButton.addEventListener('click', function() {
-                filterMenu.classList.toggle('hidden');
-            });
+            // filterButton.addEventListener('click', function() {
+            //     filterMenu.classList.toggle('hidden');
+            // });
 
-            closeButton.addEventListener('click', function() {
-                filterMenu.classList.toggle('hidden');
-            });
+            // closeButton.addEventListener('click', function() {
+            //     filterMenu.classList.toggle('hidden');
+            // });
 
-            // Handle Apply button click
-            const applyFilters = document.getElementById('applyFilters');
+            // // Handle Apply button click
+            // const applyFilters = document.getElementById('applyFilters');
 
-            applyFilters.addEventListener('click', function() {
-                let filters = {
-                    prepositives: Array.from(document.getElementById('prepositives').selectedOptions)
-                        .map(option => option.value),
-                    living_status: document.getElementById('living_status').value,
-                    social_status: document.getElementById('social_status').value,
-                    gender: document.getElementById('gender').value
-                };
+            // applyFilters.addEventListener('click', function() {
+            //     let filters = {
+            //         prepositives: Array.from(document.getElementById('prepositives').selectedOptions)
+            //             .map(option => option.value),
+            //         living_status: document.getElementById('living_status').value,
+            //         social_status: document.getElementById('social_status').value,
+            //         gender: document.getElementById('gender').value
+            //     };
 
-                // Make an AJAX request to apply the filters (adjust the URL and method as needed)
-                fetch('/path/to/filter/endpoint', {
-                        method: 'POST', // or 'GET' if needed
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(filters)
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                        // Handle the response to update the citizens list
-                        console.log(data);
-                        // Update the citizens list on the page using the response data
-                    })
-                    .catch(error => {
-                        console.error('Error applying filters:', error);
-                    });
+            //     // Make an AJAX request to apply the filters (adjust the URL and method as needed)
+            //     fetch('/path/to/filter/endpoint', {
+            //             method: 'POST', // or 'GET' if needed
+            //             headers: {
+            //                 'Content-Type': 'application/json'
+            //             },
+            //             body: JSON.stringify(filters)
+            //         })
+            //         .then(response => response.json())
+            //         .then(data => {
+            //             // Handle the response to update the citizens list
+            //             console.log(data);
+            //             // Update the citizens list on the page using the response data
+            //         })
+            //         .catch(error => {
+            //             console.error('Error applying filters:', error);
+            //         });
 
-                // Hide the filter menu after applying filters
-                filterMenu.classList.add('hidden');
-            });
+            //     // Hide the filter menu after applying filters
+            //     filterMenu.classList.add('hidden');
+            // });
         });
     </script>
 @endpush
