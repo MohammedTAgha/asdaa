1f75b32c1ea43bbfeefa473ba11665bb53e34c59
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-08-17 18:43:09 +0300

    validation is done

--- resources/views/citizens/partials/form.blade.php
+++ resources/views/citizens/partials/form.blade.php
@@ -9,19 +9,24 @@
                 <!-- ID -->
                 <div class="col-sm-3">
                     <label for="id" class="form-label">الهوية</label>
-                    <input type="text" id="id" name="id" class="form-control" required>
+                    <input type="text" id="id" name="id"
+                        class="form-control" required>
+                        <div class="invalid-feedback">رقم غير صحيح: الطول يجب ان يكون 9 ارقام</div>
+                        <div class="valid-feedback">الهوية صحيحة</div>
+                        {{-- <div id="validationResult" class="mt-3"></div> --}}
                     {{-- <div class="invalid-feedback">Invalid ID: Must be 9 digits long</div> --}}
-                    <div id="validationResult" class="mt-3"></div>
                 </div>
                 <!-- Phone -->
                 <div class="col-sm-3">
                     <label for="phone" class="form-label">رقم الهاتف</label>
-                    <input type="text" id="phone" name="phone" class="form-control">
+                    <input type="text" id="phone" name="phone"
+                        class="form-control">
                 </div>
                 <!-- Region -->
                 <div class="col-sm-3">
                     <label for="region_id" class="form-label">المنطقة</label>
-                    <select id="region_id" name="region_id" class="form-select" required>
+                    <select id="region_id" name="region_id"
+                        class="form-select" required>
                         <option value="">اختر المنطقة</option>
                     </select>
                 </div>
@@ -30,29 +35,34 @@
                 <!-- First Name -->
                 <div class="col-sm-3">
                     <label for="firstname" class="form-label">الاسم الاول</label>
-                    <input type="text" name="firstname" id="firstname" class="form-control" required>
+                    <input type="text" name="firstname" id="firstname"
+                        class="form-control" required>
                 </div>
                 <!-- Second Name -->
                 <div class="col-sm-3">
                     <label for="secondname" class="form-label">الاب</label>
-                    <input type="text" name="secondname" id="secondname" class="form-control">
+                    <input type="text" name="secondname" id="secondname"
+                        class="form-control">
                 </div>
                 <!-- Third Name -->
                 <div class="col-sm-3">
                     <label for="thirdname" class="form-label">الجد</label>
-                    <input type="text" name="thirdname" id="thirdname" class="form-control">
+                    <input type="text" name="thirdname" id="thirdname"
+                        class="form-control">
                 </div>
                 <!-- Last Name -->
                 <div class="col-sm-3">
                     <label for="lastname" class="form-label">العائلة</label>
-                    <input type="text" name="lastname" id="lastname" class="form-control" required>
+                    <input type="text" name="lastname" id="lastname"
+                        class="form-control" required>
                 </div>
             </div>
             <div class="row g-2 mt-3">
                 <!-- Social Status -->
                 <div class="col-sm-3">
                     <label for="social_status" class="form-label">الحالة الاجتماعية</label>
-                    <select id="social_status" name="social_status" class="form-select">
+                    <select id="social_status" name="social_status"
+                        class="form-select">
                         <option value="">غير محدد</option>
                         <option value="0">اعزب</option>
                         <option value="1">متزوج</option>
@@ -68,7 +78,8 @@
                 <!-- Gender -->
                 <div class="col-sm-3">
                     <label for="gender" class="form-label">الجنس</label>
-                    <select id="gender" name="gender" class="form-select" required>
+                    <select id="gender" name="gender"
+                        class="form-select" required>
                         <option value="0">ذكر</option>
                         <option value="1">انثى</option>
                     </select>
@@ -76,7 +87,8 @@
                 <!-- Date of Birth -->
                 <div class="col-sm-3">
                     <label for="date_of_birth" class="form-label">تاريخ الميلاد</label>
-                    <input type="date" id="date_of_birth" name="date_of_birth" class="form-control">
+                    <input type="date" id="date_of_birth" name="date_of_birth"
+                        class="form-control">
                 </div>
             </div>
             <div class="mt-3 d-flex justify-content-between">
@@ -95,42 +107,49 @@
                 <!-- Wife ID -->
                 <div class="col-sm-4">
                     <label for="wife_id" class="form-label">هوية الزوجة</label>
-                    <input type="text" id="wife_id" name="wife_id" class="form-control">
+                    <input type="text" id="wife_id" name="wife_id"
+                        class="form-control">
                 </div>
                 <!-- Wife Name -->
                 <div class="col-sm-6">
                     <label for="wife_name" class="form-label">اسم الزوجة</label>
-                    <input type="text" id="wife_name" name="wife_name" class="form-control">
+                    <input type="text" id="wife_name" name="wife_name"
+                        class="form-control">
                 </div>
             </div>
             <div class="row g-3 mt-3">
                 <!-- Family Members -->
                 <div class="col-sm-2">
                     <label for="family_members" class="form-label">عدد الافراد</label>
-                    <input type="number" id="family_members" name="family_members" class="form-control">
+                    <input type="number" id="family_members" name="family_members"
+                        class="form-control">
                 </div>
                 <!-- Elderly Count -->
                 <div class="col-sm-2">
                     <label for="elderly_count" class="form-label">عدد المسنين</label>
-                    <input type="number" id="elderly_count" name="elderly_count" class="form-control">
+                    <input type="number" id="elderly_count" name="elderly_count"
+                        class="form-control">
                 </div>
             </div>
             <div class="row g-3 mt-3">
                 <!-- Diseases -->
                 <div class="col-sm-2">
                     <label for="disease" class="form-label">عدد الامراض المزمنة</label>
-                    <input type="number" id="disease" name="disease" class="form-control">
+                    <input type="number" id="disease" name="disease"
+                        class="form-control">
                 </div>
                 <div class="col-sm-6">
                     <label for="disease_description" class="form-label">تفاصيل الامراض المزمنة</label>
-                    <input type="text" id="disease_description" name="disease_description" class="form-control">
+                    <input type="text" id="disease_description" name="disease_description"
+                        class="form-control">
                 </div>
             </div>
             <div class="row g-3 mt-3">
                 <!-- Obstruction -->
                 <div class="col-sm-2">
                     <label for="obstruction" class="form-label">عدد الاعاقات</label>
-                    <input type="number" id="obstruction" name="obstruction" class="form-control">
+                    <input type="number" id="obstruction" name="obstruction"
+                        class="form-control">
                 </div>
                 <div class="col-sm-6">
                     <label for="obstruction_description" class="form-label">تفاصيل</label>
@@ -153,7 +172,8 @@
                 <!-- Living Status -->
                 <div class="col-sm-3">
                     <label for="living_status" class="form-label">حالة السكن</label>
-                    <select id="living_status" name="living_status" class="form-select">
+                    <select id="living_status" name="living_status"
+                        class="form-select">
                         <option value="">غير محدد</option>
                         <option value="1">سيئ</option>
                         <option value="2">جيد</option>
@@ -163,7 +183,8 @@
                 <!-- Job -->
                 <div class="col-sm-3">
                     <label for="job" class="form-label">العمل</label>
-                    <select id="job" name="job" class="form-select">
+                    <select id="job" name="job"
+                        class="form-select">
                         <option value="">غير محدد</option>
                         <option value="1">لا يعمل</option>
                         <option value="2">عامل</option>
@@ -173,12 +194,14 @@
                 <!-- Original Address -->
                 <div class="col-sm-8">
                     <label for="original_address" class="form-label">عنوان السكن الأصلي</label>
-                    <input type="text" id="original_address" name="original_address" class="form-control">
+                    <input type="text" id="original_address" name="original_address"
+                        class="form-control">
                 </div>
                 <!-- Note -->
                 <div class="col-sm-8 mt-3">
                     <label for="note" class="form-label">ملاحظة</label>
-                    <textarea id="note" name="note" rows="3" class="form-control"></textarea>
+                    <textarea id="note" name="note" rows="3"
+                        class="form-control"></textarea>
                 </div>
             </div>
             <div class="mt-3 d-flex justify-content-between">
@@ -190,17 +213,24 @@
 </div>
 
 @push('scripts')
-    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-    <script>
+{{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+
+
+<script>
+// Validation for Palestinian ID
+document.getElementById('id').addEventListener('input', validateID);
+    document.getElementById('id').addEventListener('blur', validateID);
+
+    function validateID() {
         const idInput = document.getElementById('id');
-        const validationResult = document.getElementById('validationResult');
+        const id = idInput.value;
 
-        function validatePalestinianID(id) {
-            if (!/^\d{9}$/.test(id)) {
-                return false;
-            }
+        idInput.classList.remove('is-invalid', 'is-valid');
+        document.querySelector('#id ~ .invalid-feedback').textContent = '';
+        document.querySelector('#id ~ .valid-feedback').textContent = '';
 
+        if (id.length === 9 && /^\d{9}$/.test(id)) {
             let sum = 0;
             for (let i = 0; i < 8; i++) {
                 let digit = parseInt(id[i]);
@@ -213,31 +243,17 @@
             }
 
             let checkDigit = (10 - (sum % 10)) % 10;
-            return checkDigit === parseInt(id[8]);
-        }
-
-        function displayValidationResult(isValid) {
-            if (isValid) {
-                validationResult.innerHTML = '<div class="alert alert-success">Valid Palestinian ID</div>';
+            if (checkDigit === parseInt(id[8])) {
+                idInput.classList.add('is-valid');
+                document.querySelector('#id ~ .valid-feedback').textContent = 'ID is valid!';
             } else {
-                validationResult.innerHTML = '<div class="alert alert-danger">Invalid Palestinian ID</div>';
+                idInput.classList.add('is-invalid');
+                document.querySelector('#id ~ .invalid-feedback').textContent = 'Invalid ID!';
             }
+        } else if (id.length > 0) {
+            idInput.classList.add('is-invalid');
+            document.querySelector('#id ~ .invalid-feedback').textContent = 'Invalid ID: Must be 9 digits long';
         }
-
-        idInput.addEventListener('input', function() {
-            if (this.value.length === 9) {
-                const isValid = validatePalestinianID(this.value);
-                displayValidationResult(isValid);
-            } else {
-                validationResult.innerHTML = '';
-            }
-        });
-
-        idInput.addEventListener('blur', function() {
-            if (this.value.length > 0) {
-                const isValid = validatePalestinianID(this.value);
-                displayValidationResult(isValid);
-            }
-        });
-    </script>
+    }
+</script>
 @endpush
