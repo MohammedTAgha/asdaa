1eb859916fec4d688abcf51bfc483f98c473ef0c
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-08-18 10:40:23 +0300

    Update index.blade.php

--- resources/views/representatives/index.blade.php
+++ resources/views/representatives/index.blade.php
@@ -2,9 +2,8 @@
 
 @section('content')
     <div class="container mx-auto py-12">
-        <h1 class="text-4xl font-bold mb-4">قائمة مناديب المناطق</h1>
-        <a href="{{ route('representatives.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded">Create New
-            Representative</a>
+        <h1 class="text-4xl font-bold mb-4"> مناديب المناطق</h1>
+        <a href="{{ route('representatives.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded"> اضافة مندوب جديد </a>
         <div class="mt-6">
             <table class="min-w-full bg-white">
                 <thead class="bg-gray-800 text-white">
@@ -12,8 +11,8 @@
                         <th class="w-1/5 py-3 px-4 uppercase font-semibold text-sm">الاسم</th>
                         <th class="w-1/5 py-3 px-4 uppercase font-semibold text-sm">المنطقة</th>
                         <th class="w-1/5 py-3 px-4 uppercase font-semibold text-sm">رقم</th>
-                        <th class="w-1/5 py-3 px-4 uppercase font-semibold text-sm">Address</th>
-                        <th class="w-1/5 py-3 px-4 uppercase font-semibold text-sm">Actions</th>
+                        <th class="w-1/5 py-3 px-4 uppercase font-semibold text-sm">عدد السكان</th>
+                        <th class="w-1/5 py-3 px-4 uppercase font-semibold text-sm">اجراءات</th>
                     </tr>
                 </thead>
                 <tbody class="text-gray-700">
@@ -28,22 +27,24 @@
 
                             <td class="w-1/5 py-3 px-4">
                                 <a href="{{ route('regions.show', $representative->region->id ?? 0 ) }}">
-                                {{ $representative->region->name ??'N/A' }}
+                                {{ $representative->region->name ??'N/A'  }} - {{ $representative->region->id ??'**'  }}
                             
                             </td>
+                            
                             <td class="w-1/5 py-3 px-4">{{ $representative->phone }}</td>
-                            <td class="w-1/5 py-3 px-4">{{ $representative->address }}</td>
+                            <td class="w-1/5 py-3 px-4">{{  count($representative->region->citizens) }} اسرة</td>
+                            {{-- <td class="w-1/5 py-3 px-4">{{ $representative->address }}</td> --}}
                             <td class="w-1/5 py-3 px-4">
                                 <a href="{{ route('representatives.show', $representative->id) }}"
                                     class="text-blue-500">عرض</a>
                                 <a href="{{ route('representatives.edit', $representative->id) }}"
                                     class="text-green-500 ml-2">تعديل</a>
-                                <form action="{{ route('representatives.destroy', $representative->id) }}" method="POST"
+                                {{-- <form action="{{ route('representatives.destroy', $representative->id) }}" method="POST"
                                     class="inline">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="text-red-500 ml-2">الغاء</button>
-                                </form>
+                                </form> --}}
                             </td>
                         </tr>
                     @endforeach
