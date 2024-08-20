5503663b541bfa68460504a307165a0f22067c19
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-08-20 12:06:07 +0300

    ,,

--- resources/views/common/upload_citizens.blade.php
+++ resources/views/common/upload_citizens.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">إضافة مواطنين عن طريق ملف Excel</div>
+                <div class="card-header">إضافة الى مشروع عن طريق ملف Excel</div>
                                      
                 <div class="card-body">
                     @if (session('status'))
