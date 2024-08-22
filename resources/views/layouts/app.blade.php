9eb1d2ab666badc5f3aba6882653fb2898572a5b
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2024-08-22 13:05:08 +0300

    comment old and add new

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,17 +11,13 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta property="og:locale" content="en_US" />
 <meta property="og:type" content="article" />
-{{-- template code --}}
-
-<meta name="description" content="" />
-
 <!-- Favicon -->
 <link rel="icon"   href="{{asset('assets/img/asdaa.jpg')}}" />
 
 
+
 <!-- Fonts -->
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
 {{-- <link
     href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
     rel="stylesheet" /> --}}
@@ -31,43 +27,59 @@
 <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/tabler-icons.css') }}" />
 {{-- <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/flag-icons.css') }}" /> --}}
 
+{{-- template code for metronic--}}
+<!--begin::Page Vendor Stylesheets(used by this page)-->
+
+<link rel="stylesheet" href="{{ asset('assets/plugins/custom/prismjs/prismjs.bundle.rtl.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" />
+<!--end::Page Vendor Stylesheets-->
+<!--begin::Global Stylesheets Bundle(used by all pages)-->
+<link rel="stylesheet" href="{{ asset('assets/plugins/global/plugins.bundle.rtl.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/css/style.bundle.rtl.css') }}" />
+
+ 
+{{-- end code for metronic--}}
+
+
+
+{{-- for vuexy --}}
 <!-- Core CSS -->
-<link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/core.css') }}" class="template-customizer-core-css" />
-<link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/theme-default.css') }}"
-    class="template-customizer-theme-css" />
-<link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
+{{-- <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/core.css') }}" class="template-customizer-core-css" /> --}}
+{{-- <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/theme-default.css') }}"
+    class="template-customizer-theme-css" /> --}}
+{{-- <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" /> --}}
 
 <!-- Vendors CSS -->
-<link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/vendor/libs/node-waves/node-waves.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/vendor/libs/typeahead-js/typeahead.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/vendor/libs/bs-stepper/bs-stepper.css') }}" />
+{{-- <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" /> --}}
+{{-- <link rel="stylesheet" href="{{ asset('assets/vendor/libs/node-waves/node-waves.css') }}" /> --}}
+{{-- <link rel="stylesheet" href="{{ asset('assets/vendor/libs/typeahead-js/typeahead.css') }}" /> --}}
+{{-- <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bs-stepper/bs-stepper.css') }}" /> --}}
 
 {{-- <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap-select/bootstrap-select.css') }}" /> --}}
 {{-- <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css') }}" /> --}}
 
-<link rel="stylesheet" href="{{ asset('assets/vendor/libs/formvalidation/dist/css/formValidation.min.css') }}" />
+{{-- <link rel="stylesheet" href="{{ asset('assets/vendor/libs/formvalidation/dist/css/formValidation.min.css') }}" /> --}}
+
+
+<!-- Helpers -->
+{{-- <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script> --}}
+
+<!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+<!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
+{{-- <script src="../../assets/vendor/js/template-customizer.js"></script> --}}
+<!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+{{-- <script src="../../assets/js/config.js"></script> --}}
+
+
+{{-- <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" type="text/css" /> --}}
+{{-- <link href="{{ asset('assets/css/tailwind.min.css') }}" rel="stylesheet" type="text/css" /> --}}
+{{-- <link href="{{ asset('assets/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css" /> --}}
+{{-- <link href="{{ asset('assets/css/dataTables.tailwindcss.css') }}" rel="stylesheet" type="text/css" /> --}}
+{{-- <link href="{{ asset('assets/css/select2.min.css') }}" rel="stylesheet" type="text/css" /> --}}
 
 <!-- Page CSS and customozed 00 -->
 @yield('styles')
 @livewireStyles
-<!-- Helpers -->
-<script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
-
-<!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-<!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-<script src="../../assets/vendor/js/template-customizer.js"></script>
-<!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-<script src="../../assets/js/config.js"></script>
-
-{{-- for vuexy --}}
-<link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/tailwind.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/dataTables.tailwindcss.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
-
-{{-- custom styles --}}
 <link href="{{ asset('custom/snakbar.css') }}" rel="stylesheet" type="text/css" />
 @stack('custom_styles')
 
@@ -359,44 +371,60 @@
             showSnackbar("{{ session('warning') }}", 'warning');
         @endif
     </script>
+
+    <script>var hostUrl = "assets/";</script>
+    {{-- metronic scripts --}}
+	<!--begin::Global Javascript Bundle(used by all pages)-->
+
+    <script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
+    <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
+    <!--end::Global Javascript Bundle-->
+    <!--begin::Page Custom Javascript(used by this page)-->
+
+    <!--begin::Page Vendors Javascript(used by this page)-->
+    <script src="{{ asset('assets/js/custom/widgets.js') }}"></script>
+    <script src="{{ asset('assets/js/custom/widgets.js') }}"></script>
+
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
     <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
+    {{-- <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
     <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/node-waves/node-waves.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/node-waves/node-waves.js') }}"></script> --}}
 
-    <script src="{{ asset('assets/vendor/libs/hammer/hammer.js') }}"></script>
+    {{-- v scripts and other libs --}}
+    {{-- <script src="{{ asset('assets/vendor/libs/hammer/hammer.js') }}"></script> --}}
     {{-- <script src="{{ asset('assets/vendor/libs/i18n/i18n.js') }}"></script> --}}
-    <script src="{{ asset('assets/vendor/libs/typeahead-js/typeahead.js') }}"></script>
+    {{-- <script src="{{ asset('assets/vendor/libs/typeahead-js/typeahead.js') }}"></script> --}}
 
-    <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
+    {{-- <script src="{{ asset('assets/vendor/js/menu.js') }}"></script> --}}
     <!-- endbuild -->
 
     <!-- Vendors JS -->
-    <script src="{{ asset('assets/vendor/libs/bs-stepper/bs-stepper.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/bootstrap-select/bootstrap-select.js') }}"></script>
+    {{-- <script src="{{ asset('assets/vendor/libs/bs-stepper/bs-stepper.js') }}"></script> --}}
+    {{-- <script src="{{ asset('assets/vendor/libs/bootstrap-select/bootstrap-select.js') }}"></script> --}}
     {{-- <script src="{{ asset('assets/vendor/libs/select2/select2.js') }}"></script> --}}
-    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js') }}"></script>
+    {{-- <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js') }}"></script> --}}
 
-    <script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
+    {{-- <script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script> --}}
     <!-- Flat Picker -->
     {{-- <script src="{{ asset('assets/vendor/libs/moment/moment.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/flatpickr/flatpickr.js') }}"></script> --}}
     <!-- Form Validation -->
-    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js') }}"></script>
+    {{-- <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('assets/js/select2.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('assets/js/tables-datatables-basic.js') }}"></script> --}}
 
     <!-- Main JS -->
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+    {{-- <script src="{{ asset('assets/js/main.js') }}"></script> --}}
     <!-- Page JS -->
     @stack('scripts')
 
-    <script src="{{ asset('assets/js/select2.min.js') }}"></script>
     <!-- Page JS -->
     <script>
          $(document).ready(function() {
@@ -419,7 +447,6 @@
         @endif
 
     </script>
-    <script src="{{ asset('assets/js/tables-datatables-basic.js') }}"></script>
 </body>
 
 
