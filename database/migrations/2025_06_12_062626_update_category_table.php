d331e641c7c108a9ce2f78d46869aa75ca131233
MOHAMMED T AGHA <36562531+MohammedTAgha@users.noreply.github.com>
2025-06-12 09:32:31 +0300

    update cat table datatypes

---
+++ database/migrations/2025_06_12_062626_update_category_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('size')->nullable()->change();
+            $table->string('amount')->nullable()->change();
+            $table->string('date')->nullable()->change();
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            //
+        });
+    }
+};
